f8c860ce3426b25f83617cdae47c1acb9a64dd0c
Christian Johansen <christian@cjohansen.no>
2012-10-30 11:23:39 +0100

    Fix bogus indentation

--- lib/open_id_authentication.rb
+++ lib/open_id_authentication.rb
@@ -20,19 +20,19 @@ module OpenIdAuthentication
     store, *parameters = *([ store_option ].flatten)
 
     @@store = case store
-    when :memory
-      require 'openid/store/memory'
-      OpenID::Store::Memory.new
-    when :file
-      require 'openid/store/filesystem'
-      OpenID::Store::Filesystem.new(Rails.root.join('tmp/openids'))
-    when :memcache
-      require 'memcache'
-      require 'openid/store/memcache'
-      OpenID::Store::Memcache.new(MemCache.new(parameters))
-    else
-      store
-    end
+              when :memory
+                require 'openid/store/memory'
+                OpenID::Store::Memory.new
+              when :file
+                require 'openid/store/filesystem'
+                OpenID::Store::Filesystem.new(Rails.root.join('tmp/openids'))
+              when :memcache
+                require 'memcache'
+                require 'openid/store/memcache'
+                OpenID::Store::Memcache.new(MemCache.new(parameters))
+              else
+                store
+              end
   end
 
   self.store = nil
@@ -75,58 +75,58 @@ module OpenIdAuthentication
 
   module ControllerMethods
     protected
-      # The parameter name of "openid_identifier" is used rather than
-      # the Rails convention "open_id_identifier" because that's what
-      # the specification dictates in order to get browser auto-complete
-      # working across sites
-      def using_open_id?(identifier = nil) #:doc:
-        identifier ||= open_id_identifier
-        !identifier.blank? || request.env[Rack::OpenID::RESPONSE]
-      end
+    # The parameter name of "openid_identifier" is used rather than
+    # the Rails convention "open_id_identifier" because that's what
+    # the specification dictates in order to get browser auto-complete
+    # working across sites
+    def using_open_id?(identifier = nil) #:doc:
+      identifier ||= open_id_identifier
+      !identifier.blank? || request.env[Rack::OpenID::RESPONSE]
+    end
 
-      def authenticate_with_open_id(identifier = nil, options = {}, &block) #:doc:
-        identifier ||= open_id_identifier
+    def authenticate_with_open_id(identifier = nil, options = {}, &block) #:doc:
+      identifier ||= open_id_identifier
 
-        if request.env[Rack::OpenID::RESPONSE]
-          complete_open_id_authentication(&block)
-        else
-          begin_open_id_authentication(identifier, options, &block)
-        end
+      if request.env[Rack::OpenID::RESPONSE]
+        complete_open_id_authentication(&block)
+      else
+        begin_open_id_authentication(identifier, options, &block)
       end
+    end
 
     private
-      def open_id_identifier
-        params[:openid_identifier] || params[:openid_url]
-      end
+    def open_id_identifier
+      params[:openid_identifier] || params[:openid_url]
+    end
 
-      def begin_open_id_authentication(identifier, options = {})
-        options[:identifier] = identifier
-        value = Rack::OpenID.build_header(options)
-        response.headers[Rack::OpenID::AUTHENTICATE_HEADER] = value
-        head :unauthorized
-      end
+    def begin_open_id_authentication(identifier, options = {})
+      options[:identifier] = identifier
+      value = Rack::OpenID.build_header(options)
+      response.headers[Rack::OpenID::AUTHENTICATE_HEADER] = value
+      head :unauthorized
+    end
 
-      def complete_open_id_authentication
-        response   = request.env[Rack::OpenID::RESPONSE]
-        identifier = response.display_identifier
+    def complete_open_id_authentication
+      response   = request.env[Rack::OpenID::RESPONSE]
+      identifier = response.display_identifier
 
-        case response.status
-        when OpenID::Consumer::SUCCESS
-          yield Result[:successful], identifier,
-            OpenID::SReg::Response.from_success_response(response),
-            OpenID::AX::FetchResponse.from_success_response(response)
-        when :missing
-          yield Result[:missing], identifier, nil
-        when :invalid
-          yield Result[:invalid], identifier, nil
-        when OpenID::Consumer::CANCEL
-          yield Result[:canceled], identifier, nil
-        when OpenID::Consumer::FAILURE
-          yield Result[:failed], identifier, nil
-        when OpenID::Consumer::SETUP_NEEDED
-          yield Result[:setup_needed], response.setup_url, nil
-        end
+      case response.status
+      when OpenID::Consumer::SUCCESS
+        yield Result[:successful], identifier,
+        OpenID::SReg::Response.from_success_response(response),
+        OpenID::AX::FetchResponse.from_success_response(response)
+      when :missing
+        yield Result[:missing], identifier, nil
+      when :invalid
+        yield Result[:invalid], identifier, nil
+      when OpenID::Consumer::CANCEL
+        yield Result[:canceled], identifier, nil
+      when OpenID::Consumer::FAILURE
+        yield Result[:failed], identifier, nil
+      when OpenID::Consumer::SETUP_NEEDED
+        yield Result[:setup_needed], response.setup_url, nil
       end
+    end
   end
 
   if Rails.version >= '3'
